3c361b1af99f7371d9e7b840e7bd374998cc74d2
Mohammad Hassan <kkhaidoran@gmail.com>
2025-04-11 10:46:02 +0700

    new feat added

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ function sayName() {
   console.log("My name is " + name1);
 }
 
-
 function newFn() {
   console.log("Hey I am a new fn");
 }
 
 function isAbove18() {
   return age >= 18;
+}
+
+function newFeatureAdd() {
+  console.log("This is a new featcls");
 }
